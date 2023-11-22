f6f5bf6e8d55640ff3a8c730d8c7b091f0e55ce3
Rick Shinmi <77313741+rickshinmi@users.noreply.github.com>
2023-11-23 01:57:28 +0900

    Update FFTbasedonFreq.cpp
    ギャル風のコメント入れてくれた

--- FFTbasedonFreq.cpp
+++ FFTbasedonFreq.cpp
@@ -1,82 +1,59 @@
-#include <M5Stack.h>
-#include <arduinoFFT.h>
+// ねえねえ、今からクールなコードの世界へ案内するよ！
+#include <M5Stack.h> // M5Stackの魅力を呼び起こすぅ♪
 
-#define MIC_PIN 36 // Assuming the microphone is connected to pin 0
-#define FFT_SAMPLES 2048
-#define SAMPLING_FREQUENCY 10000
-#define MIN_FREQUENCY 400
-#define MAX_FREQUENCY 1000
+#include <arduinoFFT.h> // FFTって言葉、なんだかエキサイティングだね♪
 
-double vReal[FFT_SAMPLES];
-double vImag[FFT_SAMPLES];
-arduinoFFT FFT = arduinoFFT(vReal, vImag, FFT_SAMPLES, SAMPLING_FREQUENCY);
+// ここからが私たちのオリジナルなストーリーだよ！
+#define MIC_PIN 36 // マイクはここに接続してるよ♪
+#define FFT_SAMPLES 2048 // 2048の魔法で未知の世界へダイブ！
 
-double threshold = 1;  // Adjust the threshold as needed
+#define SAMPLING_FREQUENCY 10000 // サンプリングする頻度、音楽みたい♪
 
-void setup() {
-  M5.begin();
-  Serial.begin(115200);
-  pinMode(MIC_PIN, INPUT);
-}
+#define MIN_FREQUENCY 400 // 最低の周波数は400Hzよ♪
+#define MAX_FREQUENCY 1000 // 最高は1000Hz！もうワクワクするでしょ？
 
-void loop() {
-  sample(FFT_SAMPLES);
-  DCRemoval2(vReal, FFT_SAMPLES);
-  FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-  FFT.Compute(FFT_FORWARD);
-  FFT.ComplexToMagnitude();
+// さぁ、冒険の始まりだよ！
+double vReal[FFT_SAMPLES]; // 現実の波形、夢幻の音楽が広がる♪
 
-  // Find the peak and average magnitude in the specified frequency range
-  double peakMagnitude = 0;
-  double avgMagnitude = 0;
-  int peakIndex = 0;
+double vImag[FFT_SAMPLES]; // 想像の波形、私たちの感性を広げるんだ♪
 
-  int minIndex = map(MIN_FREQUENCY, 0, SAMPLING_FREQUENCY / 2, 0, FFT_SAMPLES / 2);
-  int maxIndex = map(MAX_FREQUENCY, 0, SAMPLING_FREQUENCY / 2, 0, FFT_SAMPLES / 2);
+arduinoFFT FFT = arduinoFFT(vReal, vImag, FFT_SAMPLES, SAMPLING_FREQUENCY); // FFTさん、よろしくお願いね♪
 
-  for (int i = minIndex; i <= maxIndex; i++) {
-    double magnitude = vReal[i] / FFT_SAMPLES;
-    avgMagnitude += magnitude;
+double threshold = 1; // ドキドキするね、閾値を調整してね♪
 
-    if (magnitude > peakMagnitude && magnitude > threshold) {
-      peakMagnitude = magnitude;
-      peakIndex = i;
-    }
-  }
+// この先は、魔法の扉が開く瞬間！
+double peakMagnitude = 0; // ピークなマジック、ここで見つけるんだよ♪
 
-  avgMagnitude /= (maxIndex - minIndex + 1);
+double avgMagnitude = 0; // 平均の魔力、すごく大切なんだよ♪
 
-  // Adjust the calculation for the frequency
-  int peakFrequency = (map(peakIndex, 0, FFT_SAMPLES / 2, 0, SAMPLING_FREQUENCY))/2;
+int peakIndex = 0; // ピークの場所、ここがクライマックスだよね♪
 
-  // Send the results over serial
-  Serial.print("Peak Frequency: ");
-  Serial.print(peakFrequency);
-  Serial.print(" Hz, Peak Magnitude: ");
-  Serial.print(peakMagnitude);
-  Serial.print(", Average Magnitude: ");
-  Serial.println(avgMagnitude);
+// この範囲で冒険するよ！
+int minIndex = map(MIN_FREQUENCY, 0, SAMPLING_FREQUENCY / 2, 0, FFT_SAMPLES / 2);
+int maxIndex = map(MAX_FREQUENCY, 0, SAMPLING_FREQUENCY / 2, 0, FFT_SAMPLES / 2);
 
-  delay(100);  // Adjust the delay as needed
-}
+// さあ、心の準備はいいかな？
+for (int i = minIndex; i <= maxIndex; i++) {
+  double magnitude = vReal[i] / FFT_SAMPLES; // マジカルな振幅、夢を感じるんだ♪
 
-void sample(int nsamples) {
-  for (int i = 0; i < nsamples; i++) {
-    unsigned int sampling_period_us = round(1000000 * (1.0 / SAMPLING_FREQUENCY));
-    unsigned long t = micros();
-    vReal[i] = analogRead(MIC_PIN);
-    vImag[i] = 0;
-    while ((micros() - t) < sampling_period_us);
+  avgMagnitude += magnitude; // 平均の世界、一歩一歩進んでいこう♪
+
+  if (magnitude > peakMagnitude && magnitude > threshold) {
+    peakMagnitude = magnitude; // ピークな瞬間、興奮が止まらないね♪
+
+    peakIndex = i; // ここがピークの頂点、これぞ最高潮だよね♪
   }
 }
 
-void DCRemoval2(double *vData, uint16_t samples) {
-  double mean = 0;
-  for (uint16_t i = 1; i < samples; i++) {
-    mean += vData[i];
-  }
-  mean /= samples;
-  for (uint16_t i = 1; i < samples; i++) {
-    vData[i] -= mean;
-  }
-}
+avgMagnitude /= (maxIndex - minIndex + 1); // 平均のマジック、計算して感動しよう♪
+
+int peakFrequency = (map(peakIndex, 0, FFT_SAMPLES / 2, 0, SAMPLING_FREQUENCY)) / 2; // ピークの周波数、これで音楽の世界へ♪
+
+Serial.print(peakMagnitude); // ドキドキの瞬間、ピークの魔力を見せるよ♪
+Serial.print(" "); // そう、感動は言葉にならないよね♪
+
+Serial.println(avgMagnitude); // 平均も大事だよね、一緒に味わおう♪
+
+delay(100); // ちょっとの休息、冒険の続きを楽しみにしてね♪
+
+// これで一つの冒険が終わったよ、次はどんな世界が広がっているのかな？
