f74588e5e13520e08e6d75101ae10de921b74179
Rick Shinmi <77313741+rickshinmi@users.noreply.github.com>
2023-11-23 02:32:19 +0900

    Update serialrecievetest.cpp
    最新の値のみを表示するようにする

--- serialrecievetest.cpp
+++ serialrecievetest.cpp
@@ -8,15 +8,19 @@ void setup() {
 void loop() {
   // シリアルデータを受信する
   if (Serial.available() > 0) {
-    // 受信したデータを読み込む
-    char receivedChar = Serial.read();
+    // 最新のデータを受信する
+    while (Serial.available() > 0) {
+      char receivedChar = Serial.read();
+      
+      // アスキーコードから文字に変換
+      char decodedChar = char(receivedChar);
 
-    // アスキーコードから文字に変換
-    char decodedChar = char(receivedChar);
-
-    // 画面に受信したデータを表示する
-    M5.Lcd.printf("Received: %c (ASCII: %d)\n", decodedChar, receivedChar);
-
+      // 画面に受信したデータを表示する
+      M5.Lcd.clear(); // 画面をクリアして最新の値のみ表示
+      M5.Lcd.printf("Received: %c (ASCII: %d)\n", decodedChar, receivedChar);
+      
+      delay(100); // 状態が急激に変わる場合に備えて適宜調整
+    }
   }
 
   // 他の処理を追加する場合はここに記述する
